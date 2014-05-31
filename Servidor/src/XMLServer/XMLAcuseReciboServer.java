3eab51238768889297e6989015c12d37363c33d2
xtianwann <xtianwann@gmail.com>
2014-05-31 17:17:21 +0200

    Modificacion XMLAcuseRecibo para que envie la idComanda

--- Servidor/src/XMLServer/XMLAcuseReciboServer.java
+++ Servidor/src/XMLServer/XMLAcuseReciboServer.java
@@ -6,15 +6,13 @@ import XML.XML;
  * @author Juan G. Pérez Leo
  * @author Cristian Marín Honor
  */
-public class XMLAcuseReciboServer extends XML{
+public class XMLAcuseReciboServer extends XML {
 
-    public XMLAcuseReciboServer(String respuesta, String explicacion) {
-        init();
-        addNodo("tipo", "AcuseRecibo", "paquete");
-        addNodo("respuesta", respuesta, "paquete");
-        if(respuesta.equals("NO")){
-            addNodo("explicacion", explicacion, "paquete");
-        }
-    }
-    
+	public XMLAcuseReciboServer(String respuesta, String explicacion) {
+		init();
+		addNodo("tipo", "AcuseRecibo", "paquete");
+		addNodo("respuesta", respuesta, "paquete");
+		addNodo("explicacion", explicacion, "paquete");
+	}
+
 }
